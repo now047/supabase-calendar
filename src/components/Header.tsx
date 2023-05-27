502a724f27bd81dfba7e44c162eae9a8be9cab63
now047 <now047@gmail.com>
2023-05-27 21:53:36 +0900

    header don't need memo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useContext, useState } from "react";
-import dayjs from "dayjs";
+import React, { useContext } from "react";
+import { Dayjs } from "dayjs";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import DraftsIcon from "@mui/icons-material/Drafts";
 import BookIcon from "@mui/icons-material/Book";
@@ -16,62 +16,47 @@ const Header = () => {
     const { tab, setTab, eventFromDate, setEventFromDate } =
         useContext(HeaderContext);
 
-    const onChange = useCallback((event: any, value: TabLabel) => {
+    const onChange = (event: any, value: TabLabel) => {
         setTab(value);
-    }, []);
+    };
 
-    const onStartDateChange = useCallback(
-        (value: dayjs.Dayjs | null, event: any) => {
-            setEventFromDate(value!);
-        },
-        []
+    const onStartDateChange = (value: Dayjs | null, event: any) => {
+        setEventFromDate(value!);
+    };
+
+    return (
+        <header>
+            <Stack direction="row" justifyContent="space-between">
+                <Tabs value={tab} onChange={onChange}>
+                    <Tab
+                        label="Calendar"
+                        value="Calendar"
+                        icon={<CalendarMonthIcon />}
+                    />
+                    <Tab
+                        label="Reservation"
+                        value="Reservation"
+                        icon={<BookIcon />}
+                    />
+                    <Tab
+                        label="Resource"
+                        value="Resource"
+                        icon={<DraftsIcon />}
+                    />
+                    <Tab label="Logout" value="Logout" icon={<LogoutIcon />} />
+                </Tabs>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Display reservations from"
+                        openTo="month"
+                        views={["year", "month"]}
+                        value={eventFromDate}
+                        onChange={onStartDateChange}
+                    />
+                </LocalizationProvider>
+            </Stack>
+        </header>
     );
-
-    const HeaderMemo = React.memo(
-        ({ tab: tab, eventFromDate: eventFromDate }: any) => {
-            return (
-                <header>
-                    <Stack direction="row" justifyContent="space-between">
-                        <Tabs value={tab} onChange={onChange}>
-                            <Tab
-                                label="Calendar"
-                                value="Calendar"
-                                icon={<CalendarMonthIcon />}
-                            />
-                            <Tab
-                                label="Reservation"
-                                value="Reservation"
-                                icon={<BookIcon />}
-                            />
-                            <Tab
-                                label="Resource"
-                                value="Resource"
-                                icon={<DraftsIcon />}
-                            />
-                            <Tab
-                                label="Logout"
-                                value="Logout"
-                                icon={<LogoutIcon />}
-                            />
-                        </Tabs>
-                        <div className="supabase-calendar-sidebar-section">
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker
-                                    label="Display reservations from"
-                                    openTo="month"
-                                    views={["year", "month"]}
-                                    value={eventFromDate}
-                                    onChange={onStartDateChange}
-                                />
-                            </LocalizationProvider>
-                        </div>
-                    </Stack>
-                </header>
-            );
-        }
-    );
-
-    return <HeaderMemo tab={tab} eventFromDate={eventFromDate} />;
 };
 
 export default Header;
